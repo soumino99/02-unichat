37107dc3f748ae8951b900ff1cf70cc4b454f650
Sota-Minokawa <soumino9@gmail.com>
2025-05-01 13:36:43 +0900

    デプロイ修正

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-import os  # 追加
+import os  
+from dotenv import load_dotenv  
+
+load_dotenv() 
 
 # アプリケーションの初期化
 app = Flask(__name__)
